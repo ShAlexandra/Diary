4b2da6d34842952701ee60fb69f341e76cbbf481
ShAlexandra <shalexandra2000@gmail.com>
2022-01-21 13:21:23 +0300

    Creating construction for the DailyItem

---
+++ app/src/main/java/com/example/diary/domain/DailyItem.kt
@@ -0,0 +1,15 @@
+package com.example.diary.domain
+
+import java.sql.Timestamp
+
+data class DailyItem(
+    val date_start: Timestamp,
+    val date_finish: Timestamp,
+    val name: String,
+    val description: String,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
--- app/src/main/java/com/example/diary/presentation/MainActivity.kt
+++ app/src/main/java/com/example/diary/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.diary
+package com.example.diary.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.diary.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
